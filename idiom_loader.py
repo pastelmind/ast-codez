156e64f27165810610789382ade96b98b46ada20
pastelmind <keepyourhonor@gmail.com>
2021-05-22 21:47:23 +0900

    Exclude string constants that contain whitespace from the idiom DB

--- idiom_loader.py
+++ idiom_loader.py
@@ -1,4 +1,5 @@
 import json
+import re
 import typing
 
 
@@ -12,9 +13,20 @@ class IdiomDatabase(typing.NamedTuple):
 def load_idioms() -> IdiomDatabase:
     with open("idioms.json", encoding="utf8") as json_file:
         raw_db = json.load(json_file)
+
+    WHITESPACE_PATTERN = re.compile(r"\s")
+
     return IdiomDatabase(
         floats=frozenset(entry["value"] for entry in raw_db["float"]),
         ints=frozenset(int(entry["value"]) for entry in raw_db["int"]),
-        strings=frozenset(entry["value"] for entry in raw_db["str"]),
+        # Strings may contain whitespace, which may prevent seq2seq from
+        # treating it as a single token.
+        # Such string literals must be normalized even if they are common
+        # idioms.
+        strings=frozenset(
+            value
+            for value in (entry["value"] for entry in raw_db["str"])
+            if not WHITESPACE_PATTERN.search(value)
+        ),
         identifiers=frozenset(entry["value"] for entry in raw_db["identifiers"]),
     )
