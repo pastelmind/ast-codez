bb5af56d5d1d69da4eacd01a08093d145145f3e8
pastelmind <keepyourhonor@gmail.com>
2021-05-23 15:55:04 +0900

    Literal statement remover uses queue instead of recursion
    It no longer raises `RecursionError` due to pathologically log
    `if..elif..else` chains.

--- ast_codez_tools/literal_statement_remover.py
+++ ast_codez_tools/literal_statement_remover.py
@@ -1,6 +1,7 @@
 import ast
 import sys
 import typing
+from collections import deque
 
 # We rely on Python 3.8+ behavior of treating all constants as ast.Constant
 if sys.version_info < (3, 8):
@@ -37,112 +38,56 @@ def is_literal_statement(node: ast.AST) -> bool:
     return False
 
 
-class LiteralStatementRemover(ast.NodeTransformer):
-    """Removes all string, bytes, and numeric constant expression statements in
-    the node.
-
-    This removes docstrings and other literals that happen to be placed at the
-    statement level. Such statements do not affect the semantics of the code.
-
-    If removing a statement would result in syntactically invalid code, this
-    inserts a pass statement to preserve the semantics of the code.
-    """
-
-    # -------- Nodes with 'body'
-
-    def _cleanup_node_body(
-        self,
-        node: typing.Union[
-            ast.FunctionDef,
-            ast.AsyncFunctionDef,
-            ast.ClassDef,
-            ast.With,
-            ast.AsyncWith,
-            ast.ExceptHandler,
-            ast.For,
-            ast.AsyncFor,
-            ast.While,
-            ast.If,
-            ast.Try,
-        ],
-    ) -> None:
-        # 'body' must not be empty
-        node.body = [child for child in node.body if not is_literal_statement(child)]
-        if not node.body:
-            node.body.append(ast.Pass())
-
-    def visit_FunctionDef(self, node: ast.FunctionDef) -> ast.AST:
-        self._cleanup_node_body(node)
-        return self.generic_visit(node)
-
-    def visit_AsyncFunctionDef(self, node: ast.AsyncFunctionDef) -> ast.AST:
-        self._cleanup_node_body(node)
-        return self.generic_visit(node)
-
-    def visit_ClassDef(self, node: ast.ClassDef) -> ast.AST:
-        self._cleanup_node_body(node)
-        return self.generic_visit(node)
-
-    def visit_With(self, node: ast.With) -> ast.AST:
-        self._cleanup_node_body(node)
-        return self.generic_visit(node)
-
-    def visit_AsyncWith(self, node: ast.AsyncWith) -> ast.AST:
-        self._cleanup_node_body(node)
-        return self.generic_visit(node)
-
-    def visit_ExceptHandler(self, node: ast.ExceptHandler) -> ast.AST:
-        self._cleanup_node_body(node)
-        return self.generic_visit(node)
-
-    # -------- Nodes with 'body' and 'orelse'
-
-    def _cleanup_node_body_and_orelse(
-        self, node: typing.Union[ast.For, ast.AsyncFor, ast.While, ast.If, ast.Try]
-    ) -> None:
-        self._cleanup_node_body(node)
-        # 'orelse' can be empty
-        node.orelse = [
-            child for child in node.orelse if not is_literal_statement(child)
-        ]
-
-    def visit_For(self, node: ast.For) -> ast.AST:
-        self._cleanup_node_body_and_orelse(node)
-        return self.generic_visit(node)
-
-    def visit_AsyncFor(self, node: ast.AsyncFor) -> ast.AST:
-        self._cleanup_node_body_and_orelse(node)
-        return self.generic_visit(node)
-
-    def visit_While(self, node: ast.While) -> ast.AST:
-        self._cleanup_node_body_and_orelse(node)
-        return self.generic_visit(node)
-
-    def visit_If(self, node: ast.If) -> ast.AST:
-        self._cleanup_node_body_and_orelse(node)
-        return self.generic_visit(node)
-
-    # -------- Nodes with 'body', 'orelse', and 'finalbody'
-
-    # ast.Try is the only node that has 'finalbody'
-    def visit_Try(self, node: ast.Try) -> ast.AST:
-        self._cleanup_node_body_and_orelse(node)
-        # 'finalbody' can be empty
-        node.finalbody = [
-            child for child in node.finalbody if not is_literal_statement(child)
-        ]
-        return self.generic_visit(node)
-
-
-_literal_statement_remover = LiteralStatementRemover()
-
-
-def remove_literal_statements(node: ast.AST) -> ast.AST:
+def remove_literal_statements(root: ast.AST) -> ast.AST:
     """Removes all literal statements (e.g. docstrings) in the node in-place.
 
     Args:
-        node: AST node to modify
+        root: AST node to modify
     Returns:
-        Modified node
+        Modified root node
     """
-    return _literal_statement_remover.visit(node)
+    for node in _ast_walk_lazy(root):
+        try:
+            if isinstance(node.body, list):
+                # 'body' must not be empty
+                node.body = [
+                    child for child in node.body if not is_literal_statement(child)
+                ] or [ast.Pass()]
+        except AttributeError:
+            pass
+
+        try:
+            if isinstance(node.orelse, list):
+                # 'orelse' can be empty
+                node.orelse = [
+                    child for child in node.orelse if not is_literal_statement(child)
+                ]
+        except AttributeError:
+            pass
+
+        try:
+            if isinstance(node.finalbody, list):
+                # 'finalbody' can be empty
+                node.finalbody = [
+                    child for child in node.finalbody if not is_literal_statement(child)
+                ]
+        except AttributeError:
+            pass
+
+    return root
+
+
+def _ast_walk_lazy(node: ast.AST) -> typing.Iterator[ast.AST]:
+    """ "Lazy" version of `ast.walk()`.
+
+    This yields the node first before iterating its child nodes.
+    This allows the caller to modify a node's children before they are iterated.
+    """
+    todo = deque([node])
+    while todo:
+        node = todo.popleft()
+        # ast.walk() adds child nodes to 'todo' before yielding 'node'.
+        # We do it backwards to avoid iterating over child nodes that have been
+        # removed by the caller.
+        yield node
+        todo.extend(ast.iter_child_nodes(node))
