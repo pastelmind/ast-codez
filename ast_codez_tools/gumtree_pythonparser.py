c536d3c7c8d035b778b5e0081ce2f9ce52c98c94
pastelmind <keepyourhonor@gmail.com>
2021-05-31 00:33:19 +0900

    gumtree_pythonparser uses xml.etree
    xml.minidom does not correctly escape newline characters in XML
    attributes. This caused pythonparser to emit invalid XML when parsing
    Python code that contains multi-line string literals.
    We solve the issue by using `xml.etree`, which is a more modern,
    Pythonic API that correctly handles newlines and is faster.

--- ast_codez_tools/gumtree_pythonparser.py
+++ ast_codez_tools/gumtree_pythonparser.py
@@ -20,7 +20,7 @@ https://github.com/GumTreeDiff/pythonparser/blob/1ff1d4d7bb9660c881d8cc590bfa44b
 # Copyright 2020-2021 Jean-Rémy Falleri <jr.falleri@gmail.com>
 
 import typing
-from xml.dom import minidom
+import xml.etree.ElementTree as ET
 
 import parso
 
@@ -35,51 +35,44 @@ def python_code_to_gumtree_xml(code: str, *, pretty: bool = False) -> str:
     """Converts Python code to XML format supported by GumTree."""
     positions = make_code_positions(code)
     parso_ast = parso.parse(code)
-
-    doc = minidom.Document()
-    parso_ast = parso.parse(code)
-    gumtree_ast = to_gumtree_node(doc, parso_ast, positions)
-    assert gumtree_ast is not None
-    doc.appendChild(gumtree_ast)
-    process_node(parso_ast, gumtree_ast, positions)
-    return doc.toprettyxml() if pretty else doc.toxml()
+    gumtree_root = to_gumtree_node(parso_ast, positions)
+    assert gumtree_root is not None
+    if pretty:
+        ET.indent(gumtree_root, space="  ")
+    return ET.tostring(gumtree_root, encoding="unicode")
 
 
-def process_node(
-    parso_node, gumtree_node: minidom.Node, positions: typing.Sequence[int]
-) -> None:
+# We use xml.etree instead of xml.minidom because it correctly escapes newline
+# characters in XML attributes, as well as being faster
+def to_gumtree_node(
+    parso_node, positions: typing.Sequence[int]
+) -> typing.Union[ET.Element, None]:
+    """Recursively builds a <tree> node from the given `parso_node`."""
+    if parso_node.type in ("keyword", "newline", "endmarker"):
+        return None
+    if parso_node.type == "operator" and parso_node.value in ".()[]:;":
+        return None
+
     if parso_node.type == "error_node":
         raise SyntaxError(f"parso failed to parse code: got {parso_node}")
-
-    for parso_child in parso_node.children:
-        gumtree_child = to_gumtree_node(
-            gumtree_node.ownerDocument, parso_child, positions
-        )
-        if gumtree_child is not None:
-            gumtree_node.appendChild(gumtree_child)
-            if hasattr(parso_child, "children"):
-                process_node(parso_child, gumtree_child, positions)
-
-
-def to_gumtree_node(
-    doc: minidom.Document, parso_node, positions: typing.Sequence[int]
-) -> typing.Union[minidom.Node, None]:
-    if parso_node.type in ("keyword", "newline", "endmarker"):
-        return
-    if parso_node.type == "operator" and parso_node.value in ".()[]:;":
-        return
-
-    gumtree_node = doc.createElement("tree")
-    gumtree_node.setAttribute("type", parso_node.type)
 
     start = positions[parso_node.start_pos[0] - 1] + parso_node.start_pos[1]
     end = positions[parso_node.end_pos[0] - 1] + parso_node.end_pos[1]
     length = end - start
-    gumtree_node.setAttribute("pos", str(start))
-    gumtree_node.setAttribute("length", str(length))
+    gumtree_node = ET.Element(
+        "tree", type=parso_node.type, pos=str(start), length=str(length)
+    )
 
-    if not hasattr(parso_node, "children") or len(parso_node.children) == 0:
-        gumtree_node.setAttribute("label", parso_node.value)
+    if parso_children := getattr(parso_node, "children", None):
+        for parso_child in parso_children:
+            gumtree_child = to_gumtree_node(parso_child, positions)
+            # Must compare explicitly with None, because ET.Element() objects
+            # behave like collections, and are falsy when they have no children.
+            if gumtree_child is not None:
+                gumtree_node.append(gumtree_child)
+    else:
+        # No children
+        gumtree_node.set("label", parso_node.value)
 
     return gumtree_node
 
